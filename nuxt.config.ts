b1e23caecc852e8f566ce3b7816085006f41dcf9
Hollosi Valentin <hollosi.valentin3@gmail.com>
2025-12-11 21:13:16 +0100

    FIX working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  devServer: {
+    host: "0.0.0.0",
+  },
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    locales: [
+      {
+        code: "hu",
+        iso: "hu-HU",
+        file: "hu.ts",
+      },
+    ],
+    langDir: "locales",
+    defaultLocale: "hu",
+    pages: {
+      index: {
+        //en: '/',
+        hu: "/",
+      },
+    },
+  },
+});
